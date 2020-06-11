63c1c9a827db3ed644579420f7e0e73a9ab4fbba
mddarr <mddarr@gmail.com>
2020-06-11 16:27:44 -0700

    Easy implementation of returning all flow data for a location git status Added a method to the repository..

--- dakobed-snotel/src/main/java/org/mddarr/dakobedsnotel/dao/SnotelRepository.java
+++ dakobed-snotel/src/main/java/org/mddarr/dakobedsnotel/dao/SnotelRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface SnotelRepository extends CrudRepository<FlowData, Integer> {
-//    List<FlowData> findByReport(Report report);
+    List<FlowData> findByLocation(Location location);
 }
--- dakobed-snotel/src/main/java/org/mddarr/dakobedsnotel/services/SnotelService.java
+++ dakobed-snotel/src/main/java/org/mddarr/dakobedsnotel/services/SnotelService.java
@@ -1,14 +1,18 @@
 package org.mddarr.dakobedsnotel.services;
 
 import org.mddarr.dakobedsnotel.api.SnotelController;
+import org.mddarr.dakobedsnotel.dao.LocationRepository;
 import org.mddarr.dakobedsnotel.dao.SnotelRepository;
 import org.mddarr.dakobedsnotel.entity.FlowData;
+import org.mddarr.dakobedsnotel.entity.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SnotelService {
@@ -16,7 +20,14 @@ public class SnotelService {
     @Autowired
     SnotelRepository snotelRepository;
 
-    public List<FlowData> getFlowDataLocation(Integer id){
+    @Autowired
+    LocationRepository locationRepository;
+
+    public List<FlowData> getFlowDataLocation(Integer id) {
+        Optional<Location> location = locationRepository.findById(id);
+        if (location.isPresent()) {
+            return snotelRepository.findByLocation(location.get());
+        }
         List<FlowData> flowData = new ArrayList<>();
         return flowData;
     }
