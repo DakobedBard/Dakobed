fe5e3e19c5bf7af59270d43fbfcf95f06d860e95
mddarr <mddarr@gmail.com>
2020-06-14 11:25:22 -0700

    Start to add the tweets pipeline..

---
+++ dakobed-orders/src/main/java/org/mddarr/dakobedorders/DakobedOrdersApplication.java
@@ -0,0 +1,13 @@
+package org.mddarr.dakobedorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakobedOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakobedOrdersApplication.class, args);
+	}
+
+}
---
+++ dakobed-products/src/main/java/org/mddarr/dakobedproducts/DakobedProductsApplication.java
@@ -0,0 +1,13 @@
+package org.mddarr.dakobedproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakobedProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakobedProductsApplication.class, args);
+	}
+
+}
