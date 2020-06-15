542f6d3c8a61ae2c50e86468f79a320a90f76dfc
mddarr <mddarr@gmail.com>
2020-06-15 14:16:33 -0700

    play w/ product thumbnail..

--- dakobed-vue/src/components/store/ProductCard.vue
+++ dakobed-vue/src/components/store/ProductCard.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     
+    <v-layout row wrap>
+        <v-flex md6>
+          <v-card dark tile flat color="error">
+            <v-card-text>#1</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex md6>
+          <v-card dark tile flat color="pink darken-4">
+            <v-card-text>#2</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout> 
+
+
     <v-card >
         <v-card-title>
             {{ productName }}
