828422bef0c07c7267c6cae464bed8ed742e779e
mddarr <mddarr@gmail.com>
2020-06-17 19:41:57 -0700

    Start working on product detail component

--- dakobed-vue/src/components/store/ProductDetail.vue
+++ dakobed-vue/src/components/store/ProductDetail.vue
@@ -7,8 +7,25 @@
           </v-card>
         </v-flex>
         <v-flex md6>
-          <v-card dark tile flat color="pink darken-4">
-            <v-card-text>#2</v-card-text>
+          <v-card  tile flat >
+            <v-flex>
+      
+                <v-card-text> <h1>{{ productName }} </h1> </v-card-text>
+            </v-flex>
+
+            <v-flex>
+     
+                <v-card-text> <h3> $ {{ price }} </h3> </v-card-text>
+
+            </v-flex>
+
+            <v-flex>
+     
+                <v-btn color="error" @click="onStyleSubmit">Add to Cart</v-btn>
+            </v-flex>
+
+              <!-- <v-text-field v-model="foo" type="number" label="Number" append-outer-icon="add"
+              @click:append-outer="increment" prepend-icon="remove" @click:prepend="decrement"></v-text-field> -->
           </v-card>
         </v-flex>
       </v-layout> 
@@ -18,7 +35,14 @@
 export default {
     created(){
         console.log('The id is: ' + this.$route.params.id);
-    }
-    
+    },
+
+    data () {
+     return {
+       productName: "Atmos 65",
+       price:"180.00"
+     }
+  },
+
 }
 </script>
