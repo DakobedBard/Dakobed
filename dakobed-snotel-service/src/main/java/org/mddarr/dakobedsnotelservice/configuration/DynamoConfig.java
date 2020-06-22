6d49d34775dcd5035f7a4d9c3cfb4e47e87a0153
mddarr <mddarr@gmail.com>
2020-06-21 20:00:34 -0700

    The snotel-service is pulling it's data from dynamo.. now I just gotta run the service on fargate..

--- dakobed-snotel-service/src/main/java/org/mddarr/dakobedsnotelservice/configuration/DynamoConfig.java
+++ dakobed-snotel-service/src/main/java/org/mddarr/dakobedsnotelservice/configuration/DynamoConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +26,7 @@ public class DynamoConfig {
     @Bean
     public AmazonDynamoDB amazonDynamoDB(){
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .withRegion("us-west-2")
                 .build();
         return client;
     }
