f48c8d731395424b40ed473147b54bc7e0cb520a
mddarr <mddarr@gmail.com>
2020-06-22 20:03:22 -0700

    run the wta scraping pipeline from docker.  docker-compose -f wta-stack-compose.yaml up --build

--- dakobed-reports/wta/wta_scrape.py
+++ dakobed-reports/wta/wta_scrape.py
@@ -5,8 +5,8 @@ import psycopg2
 
 
 def get_postgres_connection(db, schema = None):
-    conn = psycopg2.connect("host=localhost dbname={} user=postgres password=postgres".format(db)) if not schema else \
-        psycopg2.connect(host="localhost", port="5432", user="postgres",  password="postgres", database=db, options="-c search_path={}".format(schema))
+    conn = psycopg2.connect("host=postgres dbname={} user=postgres password=postgres".format(db)) if not schema else \
+        psycopg2.connect(host="postgres", port="5432", user="postgres",  password="postgres", database=db, options="-c search_path={}".format(schema))
     return conn
 
 
