fdaeb0547a7b876cdf106ef9b579756c3330ba90
mddarr <mddarr@gmail.com>
2020-06-23 11:28:23 -0700

    No longer using the vue maps plugin & using native JS api

--- dakobed-vue/src/components/pipelines/tweets/TweetsLanding.vue
+++ dakobed-vue/src/components/pipelines/tweets/TweetsLanding.vue
@@ -1,5 +1,18 @@
 <template>
 <v-container>
+    <v-layout row>
+        <v-flex md7>
+          <v-card tile flat >
+            <v-card-text>#1</v-card-text>
+            <div id="map" ref="map"></div>
+          </v-card>
+        </v-flex>
+        <v-flex md5>
+          <v-card dark tile flat color="pink darken-4">
+            <v-card-text>#2</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout> 
   <v-layout>
     <v-flex>
      <v-card>
@@ -7,22 +20,8 @@
 
         </v-card-subtitle>
          <v-card flat> 
-        <GmapMap
-      :center="center"
-      :zoom="4"
-      map-type-id="terrain"
-      style="width: 700px; height: 450px"
-    >
-    <GmapMarker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-    />
-</GmapMap>
-    </v-card>
+
+      </v-card>
      </v-card> 
 
     </v-flex>
@@ -59,6 +58,18 @@ export default {
   computed: {
     ...mapGetters(["getPipelineSelection"]),
 
+  },
+  mounted(){
+    this.map = new window.google.maps.Map(this.$refs["map"],{
+      center: {lat:40, lng:-98 },
+      zoom: 4
+    })
   }
 }
 </script>
+<style scoped>
+  #map {
+    height:600px;
+    background:gray;
+  }
+</style>
